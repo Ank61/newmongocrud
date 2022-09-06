e8858049f21b046785fbda99deebdce8874a6423
Ank61 <94075714+Ank61@users.noreply.github.com>
2022-09-06 17:47:35 +0530

    Update url.js

--- url.js
+++ url.js
@@ -1,2 +1,2 @@
 //module.exports = `mongodb://localhost:27017`
-module.exports = `mongodb+srv://admin:admin@mdb.vtkja.mongodb.net/?retryWrites=true&w=majority`
+module.exports = `mongodb+srv://ankit:ankit@cluster0.yda5k.mongodb.net/?retryWrites=true&w=majority`
